21a88e1f62dc2e6ed91770fa2387f1c165e32a16
sowmiya623 <110320161+sowmiya623@users.noreply.github.com>
2022-07-31 14:17:11 +0530

    dance studio website

---
+++ dance.php
@@ -0,0 +1,9 @@
+
+<html>
+<body>
+
+<font size="8" color="purple">Welcome</font> <?php echo $_POST["email id"]; ?><br>
+<font size="8" color="purple">soon you will receive email</font> <?php echo $_POST["Password"]; ?>
+
+</body>
+</html>
